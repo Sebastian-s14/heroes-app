a6711c3879f8316c96771058313fa003f3fb84c2
Sebastian-14 <sebas14121999@gmail.com>
2020-07-25 22:22:15 -0500

    private routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,14 +7,22 @@ import {
 
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { AuthContext } from "../auth/AuthContext";
 
 export const AppRouter = () => {
+
+    const { user } = useContext(AuthContext)
     return (
         <Router>
             <div>
                 <Switch>
                     <Route exact path='/login' component={ LoginScreen } />
-                    <Route path='/' component={ DashboardRoutes } />
+                    <PrivateRoute
+                        path='/'
+                        component={ DashboardRoutes }
+                        isAuthenticated= { user.logged }
+                    />
                 </Switch>
             </div>
         </Router>
--- src/router/DashboardRoutes.js
+++ src/router/DashboardRoutes.js
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import { MarvelScreen } from '../components/marvel/MarvelScreen'
 import { HeroScreen } from '../components/heroes/HeroScreen'
 import { DCScreen } from '../components/dc/DCScreen'
+import { SearchScreen } from "../components/search/SearchScreen";
 
 export const DashboardRoutes = () => {
     return (
@@ -14,6 +15,7 @@ export const DashboardRoutes = () => {
                     <Route exact path="/marvel" component={ MarvelScreen }/>
                     <Route exact path="/hero/:heroeId" component={ HeroScreen }/>
                     <Route exact path="/dc/" component={ DCScreen }/>
+                    <Route path='/search' component={ SearchScreen } />
                     <Redirect to="/marvel"/>
                 </Switch>
             </div>
